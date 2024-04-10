93bc85549aa3b48577412047dbd0a0d0ac1de815
Nikola Trajkovic <trajkovic.nikola2001@gmail.com>
2024-04-11 01:44:37 +0200

    napravljena klasa magacin i implementirane metode interfejsa

---
+++ magacin/src/magacin/Magacin.java
@@ -0,0 +1,25 @@
+package magacin;
+
+import magacin.interfejs.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs {
+
+	@Override
+	public void dodajArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pretraziArtikal(String naziv) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
