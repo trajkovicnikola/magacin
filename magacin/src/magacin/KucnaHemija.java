d0fc1c4b6e98338281dde401a676bf1794117b3e
Nikola Trajkovic <trajkovic.nikola2001@gmail.com>
2024-04-11 01:07:10 +0200

    Napravljene klase, atributi, get i set metode

---
+++ magacin/src/magacin/KucnaHemija.java
@@ -0,0 +1,16 @@
+package magacin;
+
+import java.util.Date;
+
+public class KucnaHemija extends Artikal {
+
+	private Date datum;
+
+	public Date getDatum() {
+		return datum;
+	}
+
+	public void setDatum(Date datum) {
+		this.datum = datum;
+	}
+}
